7ae2a228bf4a26556a3ff8fe62096c0bec9debb2
nasuton <lovetennis_airu@yahoo.co.jp>
2016-11-07 17:31:44 +0900

    no message

---
+++ Assets/Scripts/Katou/enemy_state.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_state : MonoBehaviour {
+
+    private bool isDed;
+
+    private bool isDamage;
+
+   	// Use this for initialization
+	void Start ()
+    {
+        isDed = false;
+        isDamage = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(isDed)
+        {
+            return;
+        }
+	}
+
+
+
+}
