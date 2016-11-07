33c432faee3e4ae92dc24349b855673948d73741
nasuton <lovetennis_airu@yahoo.co.jp>
2016-11-07 12:58:24 +0900

    no message

---
+++ Assets/Scripts/Katou/status.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class status : MonoBehaviour
+{
+    public bool isDed;
+
+    private bool isDamage;
+
+    [SerializeField]
+    private float maxhp;
+
+    public float now_hp;
+
+    void Start()
+    {
+        isDamage = false;
+        isDed = false;
+        now_hp = maxhp;
+    }
+
+    void Update()
+    {
+
+    }
+	
+}
